41d36dfc3a5e955bbe44020bca10e748227bb96b
fossdd <fossdd@tutanota.com>
2021-04-09 11:10:14 +0100

    Fix regex

--- save.sh
+++ save.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-REGEX='(?<=>                    )(\s*.*)(?=                    <\/a>)'
+REGEX='(?s)(?<=http:\/\/itunes.apple.com\/WebObjects\/MZSearch.woa\/wa\/advancedSearchResults\?artistTerm=)(.*?)(?=&uo=8&at=11lK3m)'
 STATIONS=( "technobase" "housetime" "hardbase" "trancebase" "coretime" "clubtime" "teatime" "replay" )
 
 for station in "${STATIONS[@]}"
@@ -9,7 +9,7 @@ do
 	zcmlimitstart=0
 	for i in {1..200}; do
 		url=$baseurl?zcmlimitstart=$zcmlimitstart
-		curl $url | grep "$REGEX" -P -o | while read -r line ; do
+		curl $url | grep "$REGEX" -P -o | sed -e 's/&songTerm=/ - /g' | while read -r line ; do
 			echo $line[station=$station] | tee -a list
 		done
 		zcmlimitstart=`expr $zcmlimitstart + 15`
